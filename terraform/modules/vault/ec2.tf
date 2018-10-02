d55180730eb688b6bf65b98418f0df8bc9473053
Sean Lingren <srlingren@gmail.com>
2018-10-01 21:36:31 -0400

    combine asg/lc

--- terraform/modules/vault/ec2.tf
+++ terraform/modules/vault/ec2.tf
@@ -2,8 +2,33 @@
 #
 # Copyright (c) 2014-2018 Avant, Sean Lingren
 
+resource "aws_launch_configuration" "lc" {
+  name_prefix          = "${ var.name_prefix }-"
+  image_id             = "${ var.ami_id }"
+  instance_type        = "${ var.instance_type }"
+  key_name             = "${ var.ssh_key_name }"
+  iam_instance_profile = "${ aws_iam_instance_profile.vault_ec2_instance_profile.id }"
+  user_data            = "${ data.template_file.userdata.rendered }"
+
+  ebs_optimized               = false
+  enable_monitoring           = false
+  associate_public_ip_address = false
+
+  security_groups = ["${ aws_security_group.vault_sg_in_ec2.id }"]
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = "100"
+    delete_on_termination = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_autoscaling_group" "asg" {
-  name = "${ var.name_prefix }"
+  name = "${ var.name_prefix }-"
 
   launch_configuration = "${ aws_launch_configuration.lc.name }"
   vpc_zone_identifier  = ["${ var.ec2_subnets }"]
