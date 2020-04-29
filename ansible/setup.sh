2f2c5b7247d800797b3c55a9cf87153070eee558
David Cox <david@joulex.com>
2020-04-29 15:26:17 -0400

    Added ansibl scripting

---
+++ ansible/setup.sh
@@ -0,0 +1,1 @@
+ansible vault -i inventory --private-key ~/.ssh/vault-ssh-key.pem -u ec2-user -m setup
--- scripts/ldap/login.sh
+++ scripts/ldap/login.sh
@@ -1,8 +1,21 @@
+dir=$(dirname $0)
+. $dir/../vault.inc
+
 if [[ -z $VAULT_USER ]] ; then
     echo "Export VAULT_USER to your full user name in Okta/LDAP"
     exit 1
 fi
 
-set -x
-vault login -method=ldap username="$VAULT_USER"
+result=$(vault login -method=ldap username="$VAULT_USER")
+token=$(echo "$result" | awk '
+    /^token[    ]/ {
+        print( $2 )
+    }
+')
 
+echo "$result" 
+
+if [[ -n $token ]] ; then
+    echo ""
+    echo "export VAULT_TOKEN=$token"
+fi
