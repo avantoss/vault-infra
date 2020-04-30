01045d84b1be71fb7afb5c6df727b9cf67cf7c30
David Cox <david@joulex.com>
2020-04-30 12:33:08 -0400

    Added host to audit log

---
+++ scripts/audit/disable.sh
@@ -0,0 +1,1 @@
+vault audit disable file 
--- scripts/list-users.sh
+++ scripts/list-users.sh
@@ -1,4 +1,4 @@
 curl \
     --header "X-Vault-Token: $VAULT_TOKEN" \
     --request LIST \
-    http://127.0.0.1:8200/v1/auth/ldap/users
+    $VAULT_ADDR/v1/auth/ldap/users
