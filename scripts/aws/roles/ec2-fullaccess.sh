22ed6d06c1a24ef7311d07ec29f4f7b26bf7d696
David Cox <david@joulex.com>
2020-05-05 21:08:31 -0400

    Fixed ec2-readonly

---
+++ scripts/aws/roles/ec2-fullaccess.sh
@@ -0,0 +1,14 @@
+vault write aws/roles/ec2-fullaccess \
+    credential_type=iam_user \
+    policy_document=-<<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "ec2:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
--- scripts/aws/roles/ec2-readonly.sh
+++ scripts/aws/roles/ec2-readonly.sh
@@ -1,15 +1,3 @@
 vault write aws/roles/ec2-readonly \
-    policy_arns=arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess,arn:aws:iam::aws:policy/IAMReadOnlyAccess \
-    credential_type=iam_user \
-    policy_document=-<<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "ec2:*",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+    policy_arns=arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess \
+    credential_type=iam_user 
