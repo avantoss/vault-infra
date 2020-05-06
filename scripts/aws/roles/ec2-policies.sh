c967051f9ebe054f28c13be578b09cd5d6c36052
David Cox <david@joulex.com>
2020-05-06 16:17:56 -0400

    Rename

--- scripts/aws/roles/ec2-policies.sh
+++ scripts/aws/roles/ec2-policies.sh
@@ -12,3 +12,7 @@ vault write aws/roles/ec2-fullaccess \
   ]
 }
 EOF
+
+vault write aws/roles/ec2-readonly \
+    policy_arns=arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess \
+    credential_type=iam_user 
--- scripts/aws/roles/ec2-readonly.sh
+++
@@ -1,3 +0,0 @@
-vault write aws/roles/ec2-readonly \
-    policy_arns=arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess \
-    credential_type=iam_user 
