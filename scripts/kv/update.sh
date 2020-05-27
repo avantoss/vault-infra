45328f20a4b289e637b99c1b556a33e12f07d828
David Cox <david@joulex.com>
2020-05-27 11:59:06 -0400

    Added kv update script

---
+++ scripts/kv/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -z $2 ]] ; then
+    echo "Usage: $0 <path> <key=value>..."
+    exit 1
+fi
+path=$1
+shift
+
+set -x
+dir=$(dirname $0)
+current=$($dir/pairs.sh -p $path -s)
+if [[ -n $current ]] ; then
+    $dir/put.sh $path $current "$@"
+fi
