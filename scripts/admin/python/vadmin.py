9cb0e2152d4e57a2790e89ec51e36d48e7f551c8
David Cox <david@joulex.com>
2020-06-01 17:42:03 -0400

    Refactored and added generate-root commands

--- scripts/admin/python/vadmin.py
+++ scripts/admin/python/vadmin.py
@@ -14,6 +14,8 @@ import os.path
 import gnupg
 import yu
 import base64
+import getpass
+
 from pathlib import Path
 
 log = logging.getLogger( "vadmin" )
@@ -21,10 +23,27 @@ log = logging.getLogger( "vadmin" )
 here = os.path.dirname(os.path.realpath(__file__))
 root = Path(here).parent
 
-key_line = re.compile( "^Key \d+: (.*)")
+key_line = re.compile( "^Key \d+ (.*)")
 
 def b64dec( v ):
     return base64.b64decode(v) if v is not None else None
+
+def run( cmd, inp=None, out=None ):
+    p = subprocess.Popen(cmd, stdin=subprocess.PIPE if inp else None, stdout=subprocess.PIPE if out else None)
+    out, err = p.communicate(input=inp.encode('utf-8') if inp else None)
+    log.debug( "run: command: {}, input: {}, output: {}".format(cmd, inp, out) )
+    if p.returncode != 0:
+        raise Exception("Failed executing: {}: {}".format(cmd, p.returncode))
+
+    od = out.decode('utf-8') if out else None
+    log.debug( "run: output: {}".format(od) )
+    return od, err
+
+
+def vault( *args ):
+    cmd = [shutil.which('vault'), 'operator' ]
+    cmd.extend( args )
+    return cmd
 
 def get_opts():
     cli = argparse.ArgumentParser(
@@ -36,6 +55,7 @@ def get_opts():
     subs = cli.add_subparsers()
     add_gpg_options( subs )
     add_rekey_options( subs )
+    add_root_options( subs )
 
     return cli
 
@@ -61,16 +81,27 @@ def add_rekey_options( subs ):
     spp.add_argument('--nonce', '-n', required=True, help="nonce from rekey -init")
     spp.add_argument('--key', '-k', help="key plaintext")
     spp.add_argument('--file', '-f', help="encrypted keyfile")
-    spp.add_argument('--test', '-t', action='store_true', help="run code through test file to mimic real rekey")
     spp.set_defaults(fn=process_rekey_add)
 
     spp = sps.add_parser('verify', help='verify a new key to the rekey operation')
-    spp.add_argument('--config', '-c', default=os.path.join(root, "vadmin.yml"), help="number of shards required to unseal")
     spp.add_argument('--nonce', '-n', required=True, help="nonce from rekey -init")
     spp.add_argument('--key', '-k', help="key plaintext")
     spp.add_argument('--file', '-f', help="encrypted keyfile")
-    spp.add_argument('--test', '-t', action='store_true', help="run code through test file to mimic real rekey")
     spp.set_defaults(fn=process_rekey_verify)
+
+def add_root_options( subs ):
+    sps = subs.add_parser('root', help='Generate root operations').add_subparsers()
+
+    spp = sps.add_parser('init', help='initialize the new root key process')
+    spp.add_argument('--key', '-k', help="key plaintext")
+    spp.add_argument('--file', '-f', help="encrypted keyfile")
+    spp.set_defaults(fn=process_root_init)
+
+    spp = sps.add_parser('add', help='verify a new key to the rekey operation')
+    spp.add_argument('otp', help="one-time password")
+    spp.add_argument('--key', '-k', help="key plaintext")
+    spp.add_argument('--file', '-f', help="encrypted keyfile")
+    spp.set_defaults(fn=process_root_add)
 
 
 def get_user_key( user, keys ):
@@ -103,7 +134,6 @@ def with_user_key_files( users, handler ):
             log.debug( "key: {}".format(key) )
             filename = "{}.asc".format(user.replace('.','_').replace('@','_').replace( '__','_') )
             file = os.path.join( tmp, filename )
-            # file = filename
             log.debug( "Writing: {}".format(filename) )
             with open( file, "w" ) as fd:
                 fd.write( asc )
@@ -114,46 +144,24 @@ def with_user_key_files( users, handler ):
 def load_config( config ):
     return yu.yaml_load_file( config )
 
-def run_command_and_parse_keys( cmd, input ):
-    log.debug( "run_command_and_parse_keys: command: {}, input: {}".format(cmd,input) )
-    p = subprocess.Popen(cmd, stdin=subprocess.PIPE if input else None, stdout=subprocess.PIPE )
-    out, err = p.communicate(input=input)
-    if p.returncode != 0:
-        raise Exception("Failed executing: {}: {}".format(cmd, p.returncode))
-
+def run_command_and_parse_keys( cmd, inp ):
+    log.debug( "run_command_and_parse_keys: command: {}, input: {}".format(cmd,inp) )
+    out, err = run( cmd, inp, True )
     keys = list()
-    for line in out.decode("utf-8").splitlines():
+    for line in out.splitlines():
         print( line )
         match = key_line.match(line)
         if match:
             keys.append( match.group(1) )
     return keys
 
-def process_rekey_init( config, **_ ):
-    cfg = load_config( config )
-    threshold = cfg['threshold']
-    def handler( user_key_files ):
-        shares = len(user_key_files)
-        keys_arg = ','.join([ file for user, file in user_key_files ] )
-        cmd = [ shutil.which('vault'), 'operator', 'rekey', '-init', '-verify', '-backup',
-            '-key-shares={}'.format(shares),
-            '-key-threshold={}'.format(threshold),
-            '-pgp-keys={}'.format(keys_arg)
-        ]
-        log.debug( "process_rekey_init: command: {}".format(cmd) )
-        p = subprocess.Popen(cmd)
-        p.communicate()
-        if p.returncode != 0:
-            raise Exception("Failed initializing rekey: {} : {}".format(cmd, p.returncode))
-
-    with_user_key_files( cfg['users'], handler )
-
 def get_passphrase():
     passphrase = None
     while not passphrase:
-        print("Entry GPG passphrase > ", end='', file=sys.stderr)
-        sys.stderr.flush()
-        passphrase = sys.stdin.readline().strip()
+        try:
+            passphrase = getpass.getpass(prompt='Entry GPG passphrase > ')
+        except Exception as error:
+            print('ERROR', error, file=sys.stderr)
 
     return passphrase
 
@@ -172,20 +180,33 @@ def get_decrypted_key( file ):
 
         return ret
 
-def process_rekey_add( config, key, file, nonce, test, **_ ):
+def get_user_key( file, key ):
+    inp = get_decrypted_key( file ) if file else key
+    if not inp: raise Exception( "No decryption key/file provided")
+    return inp
+
+def process_rekey_init( config, **_ ):
+    cfg = load_config( config )
+    threshold = cfg['threshold']
+    def handler( user_key_files ):
+        shares = len(user_key_files)
+        keys_arg = ','.join([ file for user, file in user_key_files ] )
+        cmd = vault( 'rekey', '-init', '-verify', '-backup',
+            '-key-shares={}'.format(shares),
+            '-key-threshold={}'.format(threshold),
+            '-pgp-keys={}'.format(keys_arg)
+        )
+        log.debug( "process_rekey_init: command: {}".format(cmd) )
+        run( cmd )
+
+    with_user_key_files( cfg['users'], handler )
+
+def process_rekey_add( config, key, file, nonce, **_ ):
     cfg = yu.yaml_load_file( config )
     users = cfg['users']
-    if test:
-        cmd = [ shutil.which("cat"), os.path.join(root,"rekey.log") ]
-        input = None
-    else:
-        cmd = [ shutil.which("vault"), 'operator', 'rekey', '-nonce={}'.format(nonce), '-' ]
-        if file:
-            input = get_decrypted_key( file )
-        elif key:
-            input = key
-
-    keys = run_command_and_parse_keys( cmd, input.encode('utf-8') )
+    cmd = vault( 'rekey', '-nonce={}'.format(nonce), '-' )
+    inp = get_user_key( file, key )
+    keys = run_command_and_parse_keys( cmd, inp )
 
     if keys:
         if len(keys) == len(users):
@@ -197,23 +218,24 @@ def process_rekey_add( config, key, file, nonce, test, **_ ):
         log.info( "No keys returned" )
 
 
-def process_rekey_verify( config, key, file, nonce, test, **_ ):
-    cfg = yu.yaml_load_file( config )
-    users = cfg['users']
-    if test:
-        cmd = [ shutil.which("cat"), os.path.join(root,"rekey.log") ]
-        input = None
-    else:
-        cmd = [ shutil.which("vault"), 'operator', 'rekey', '-verify', '-nonce={}'.format(nonce), '-' ]
-        if file:
-            input = get_decrypted_key( file )
-        elif key:
-            input = key
+def process_rekey_verify( key, file, nonce, **_ ):
+    cmd = vault( 'rekey', '-verify', '-nonce={}'.format(nonce), '-' )
+    inp = get_user_key( file, key )
+    out, err = run( cmd, inp )
 
-    p = subprocess.Popen(cmd, stdin=subprocess.PIPE )
-    out, err = p.communicate(input=input.encode('utf-8'))
-    if p.returncode != 0:
-        raise Exception("Failed executing: {}: {}".format(cmd, p.returncode))
+def process_root_init( key, file, **_ ):
+    inp = get_user_key( file, key )
+    cmd = vault( 'generate-root', '-generate-otp' )
+    otp, err = run( cmd, out=True )
+    otp = otp.strip()
+    cmd = vault( 'generate-root', '-init', '-otp={}'.format(otp), '-' )
+    run( cmd, inp )
+    print( "One time password: {}".format(otp) )
+
+def process_root_add( key, file, otp, **_ ):
+    inp = get_user_key( file, key )
+    cmd = vault( 'generate-root', '-otp={}'.format(otp) )
+    run( cmd, inp )
 
 def process_gpg_decrypt( file, **_ ):
     decrypted = get_decrypted_key( file )
